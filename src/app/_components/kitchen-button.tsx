2065b57d5171d5435a4bca7f20790075cc9d8d82
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2025-12-11 21:36:17 +0200

    fix(KitchenButton): router path corrected

--- src/app/_components/kitchen-button.tsx
+++ src/app/_components/kitchen-button.tsx
@@ -13,7 +13,7 @@ export const KitchenButton = React.forwardRef<HTMLButtonElement, React.ButtonHTM
     className="fixed top-4 right-4 z-50 p-3 text-white rounded-full shadow-lg transition-all active:shadow-xl active:scale-105 bg-neutral-800 active:bg-neutral-900"
     ref={ref}
     {...props}
-    onClick={() => { router.push("/kitchen") }}
+    onClick={() => { router.push("/kitchen-view") }}
     >
     <ChefHat className="w-6 h-6" />
   </button>
