6ecce9990a661d5c318adad7eecd69e8bd9afcfc
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2025-12-19 14:51:10 +0200

    feat(SeatSelection): Button transition

--- src/app/seat-selection/page.tsx
+++ src/app/seat-selection/page.tsx
@@ -103,9 +103,11 @@ export default function SeatSelection() {
           <button
             type="submit"
             disabled={isDisabled}
-            className="py-3 mt-5 w-full text-white bg-green-600 rounded-lg transition-colors active:bg-green-700 disabled:bg-neutral-400"
+            className="py-3 mt-5 w-full text-white bg-green-600 rounded-lg transition-all active:bg-green-700 active:scale-105 group disabled:bg-neutral-400"
           >
-            Continue
+            <span className="group-active:scale-105 group-active:text-neutral-400">
+              Continue
+            </span>
           </button>
         </form>
       </div>
