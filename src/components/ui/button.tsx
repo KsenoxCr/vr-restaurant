17c6229eaa194394a0df55850037e492422138c7
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2025-12-19 21:55:06 +0200

    refactor: cva for app wide style system

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,72 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { type ButtonHTMLAttributes, forwardRef } from "react";
+import { cn } from "~/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center transition-all disabled:cursor-not-allowed disabled:opacity-50 group",
+  {
+    variants: {
+      variant: {
+        primary:
+          "bg-green-600 text-white active:bg-green-700 active:text-neutral-400",
+        secondary:
+          "bg-neutral-800 text-white active:bg-neutral-900 active:text-neutral-400",
+        destructive:
+          "bg-neutral-700 text-white hover:bg-red-600 active:bg-red-700 active:text-neutral-400",
+        increment:
+          "bg-green-600 text-white active:bg-green-700 active:text-neutral-400",
+        decrement:
+          "bg-neutral-900 text-white active:bg-neutral-950 active:text-neutral-400",
+        ghost: "text-green-600 active:text-green-700",
+        toggle:
+          "bg-neutral-800 text-neutral-300 data-[active=true]:bg-green-600 data-[active=true]:text-neutral-800 shadow-mb",
+      },
+      size: {
+        default: "px-4 py-3",
+        sm: "px-3 py-2 text-sm",
+        lg: "px-6 py-4 text-lg",
+        icon: "w-8 h-8",
+        "icon-lg": "w-10 h-10 text-3xl",
+      },
+      rounded: {
+        default: "rounded-lg",
+        full: "rounded-full",
+        xl: "rounded-xl",
+      },
+      active: {
+        true: "active:scale-105",
+        false: "",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "default",
+      rounded: "default",
+      active: true,
+    },
+  },
+);
+
+export interface ButtonProps
+  extends
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, rounded, active, ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          buttonVariants({ variant, size, rounded, active, className }),
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
