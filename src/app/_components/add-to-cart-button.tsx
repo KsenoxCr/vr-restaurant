e2a3c85aced74af958c578903237a2f5a7239baa
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2025-12-19 20:53:43 +0200

    feat(AddToCartButton): Returns back to menu

--- src/app/_components/add-to-cart-button.tsx
+++ src/app/_components/add-to-cart-button.tsx
@@ -4,6 +4,7 @@ import { ShoppingCart } from "lucide-react";
 import { useState, useEffect } from "react";
 import { formatCents } from "~/utils/price";
 import { useCartStore } from "~/stores/cart-store";
+import { useRouter } from "next/navigation";
 
 export function AddToCartButton({
   id,
@@ -20,6 +21,8 @@ export function AddToCartButton({
 }) {
   const [total, setTotal] = useState(priceCents);
   const addItem = useCartStore((state) => state.addItem);
+
+  const router = useRouter();
 
   useEffect(() => {
     setTotal(priceCents * quantity);
@@ -38,7 +41,10 @@ export function AddToCartButton({
   return (
     <div className="flex justify-center items-center mx-4 mb-2">
       <button
-        onClick={handleAddToCart}
+        onClick={() => {
+          handleAddToCart();
+          window.history.length > 0 ? router.back() : router.push("/menu-view");
+        }}
         className="flex gap-2 justify-center items-center p-4 mb-4 w-full bg-green-600 rounded-xl shadow-lg transition-all active:bg-green-700 active:scale-105 group active:text-neutral-400"
       >
         <ShoppingCart className="w-6 h-6 transition-all group-active:-translate-x-1 group-active:scale-105" />
