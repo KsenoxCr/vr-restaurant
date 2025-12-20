1544d34d59d4dd2deef60e15def8d6352bd4a0ee
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2025-12-20 12:54:10 +0200

    fix(CartButton): transition timing mismatch

--- src/app/_components/cart-button.tsx
+++ src/app/_components/cart-button.tsx
@@ -11,9 +11,9 @@ export function CartButton() {
   return (
     <Button variant="ghost" active={false} className="relative" asChild>
       <Link href="/cart">
-        <ShoppingCart className="w-8 h-8" />
+        <ShoppingCart className="w-8 h-8 transition-colors" />
         {itemCount > 0 && (
-          <span className="flex absolute bottom-7 left-8 justify-center items-center p-1 text-xs font-semibold text-white bg-green-600 rounded-full min-w-6 group-active:bg-green-700 group-active:text-neutral-400">
+          <span className="flex absolute bottom-7 left-8 justify-center items-center p-1 text-xs font-semibold text-white bg-green-600 rounded-full transition-colors min-w-6 group-active:bg-green-700 group-active:text-neutral-400">
             {itemCount}
           </span>
         )}
