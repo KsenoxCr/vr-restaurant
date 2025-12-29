cf7a696dea81c40fdc3a976b417f18ac118accf0
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2025-12-29 18:50:55 +0200

    refactor(ErrorScreen): Fallback when no href on callback prop & visibility hook

--- src/app/_components/screen/error-screen.tsx
+++ src/app/_components/screen/error-screen.tsx
@@ -1,42 +1,75 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "~/app/_components/ui/button";
 import { Text } from "../ui/text";
+import { useState } from "react";
 
 type ClickOrHref = {
   message?: string;
-  error?: Error & { digest?: string };
-} & ({ callback: () => void } | { href: string; label?: string });
+  errorMessage?: string;
+  digest?: string;
+} & (
+  | { callback: () => void; label?: string }
+  | { href: string; label?: string }
+  | { label: string }
+);
 
 export function ErrorScreen(props: ClickOrHref) {
-  return (
-    <div className="flex fixed inset-0 flex-col justify-center items-center bg-gray">
-      <Text as="h1" size="lg" weight="bold">
-        {props.message ? props.message : "Something went wrong..."}
-      </Text>
-      {props.error && (
-        <Text as="p" variant="error" className="mt-4">
-          {props.error.message}
-        </Text>
-      )}
-      {props.error?.digest && (
-        <Text as="p" size="xs" className="mt-2">
-          {props.error.digest}
-        </Text>
-      )}
-      {"callback" in props ? (
+  const [visible, setVisible] = useState(true); // Destroy instead of set invisible if possible dead components accumulating
+
+  const displayButton = () => {
+    if ("callback" in props) {
+      return (
         <Button
           variant="secondary"
           active={true}
           className="mt-5"
           onClick={() => props.callback()}
         >
-          Reset
+          {props.label ?? "Reset"}
         </Button>
-      ) : (
+      );
+    }
+    if ("href" in props) {
+      return (
         <Button variant="secondary" active={true} className="mt-5" asChild>
           <Link href={props.href}>{props.label ?? "Go Back"}</Link>
         </Button>
+      );
+    }
+
+    return (
+      <Button
+        onClick={() => setVisible(false)}
+        variant="secondary"
+        active={true}
+        className="mt-5"
+        asChild
+      ></Button>
+    );
+  };
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div className="flex fixed inset-0 flex-col justify-center items-center bg-gray">
+      <Text as="h1" size="lg" weight="bold">
+        {props.message ? props.message : "Something went wrong..."}
+      </Text>
+      {props.errorMessage && (
+        <Text as="p" variant="error" className="mt-4">
+          {props.errorMessage}
+        </Text>
       )}
+      {props.digest && (
+        <Text as="p" size="xs" className="mt-2">
+          {props.digest}
+        </Text>
+      )}
+      {displayButton()}
     </div>
   );
 }
