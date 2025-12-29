02d793d290ad12294c6c025308ac26b0a0c59319
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2025-12-29 19:01:42 +0200

    fix: colorway following style system

--- src/app/_components/screen/loading-page.tsx
+++ src/app/_components/screen/loading-page.tsx
@@ -2,8 +2,8 @@ import { LoaderCircle } from "lucide-react";
 
 export function LoadingPage() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-light-gray">
-      <LoaderCircle className="w-20 h-20 animate-spin text-dark-gray" />
+    <div className="flex flex-col justify-center items-center h-screen bg-gray">
+      <LoaderCircle className="w-20 h-20 animate-spin text-dark" />
     </div>
   );
 }
