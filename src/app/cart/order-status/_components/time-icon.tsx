ef5ea638e7e36bd6533d02110c7d5e7dc4741201
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2026-01-03 15:06:56 +0200

    feat: TimeIcon

---
+++ src/app/cart/order-status/_components/time-icon.tsx
@@ -0,0 +1,9 @@
+import { Clock4 } from "lucide-react";
+
+export function TimeIcon() {
+  return (
+    <div className="p-4 rounded-full bg-dark-gray">
+      <Clock4 className="w-14 h-14 text-light-gray" />
+    </div>
+  );
+}
