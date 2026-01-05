fc00242d73f77cf4b8ef0dccadb16f944fff6767
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2026-01-05 16:04:33 +0200

    feat: order status views

---
+++ src/app/cart/order-status/_components/order-summary.tsx
@@ -0,0 +1,38 @@
+import { Typography } from "~/app/_components/ui/typography";
+import { formatCents } from "~/lib/utils/price";
+import { useOrderStore } from "~/stores/order-store";
+
+export function OrderSummary({ idOnly = false }: { idOnly?: boolean }) {
+  const id = useOrderStore((state) => state.id);
+  const items = useOrderStore((state) => state.items);
+  const getTotalPrice = useOrderStore((state) => state.getTotalPrice);
+
+  if (idOnly) {
+    return (
+      <Typography className="mb-1" variant="muted" size="sm">
+        Order #{id}
+      </Typography>
+    );
+  }
+
+  return (
+    <div className="flex w-[80%] flex-col gap-1 rounded-lg bg-dark-gray p-4">
+      <Typography className="mb-1" variant="muted" size="sm">
+        Order #{id}
+      </Typography>
+      {items.map((item) => (
+        <div className="grid items-center grid-cols-[2fr_1fr_1fr]">
+          <Typography className="">{item.name}</Typography>
+          <Typography className="">× {item.quantity}</Typography>
+          <Typography className="text-right" variant="muted">
+            {formatCents(item.priceCents)}
+          </Typography>
+        </div>
+      ))}
+      <div className="flex justify-between mt-4">
+        <Typography>Total</Typography>
+        <Typography variant="price">{formatCents(getTotalPrice())}</Typography>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/cart/order-status/order-status-view.tsx
@@ -0,0 +1,132 @@
+import { OrderStatus } from "@prisma/client";
+import { Typography } from "~/app/_components/ui/typography";
+import { OrderSummary } from "./_components/order-summary";
+import { Button } from "~/app/_components/ui/button";
+import Link from "next/link";
+import {
+  ClockIcon,
+  EyeIcon,
+  ChefHatIcon,
+  SoupIcon,
+  CircleCheckIcon,
+  BanIcon,
+  XIcon,
+} from "./_components/status-icons";
+import { useRouter } from "next/navigation";
+import { useOrderStore } from "~/stores/order-store";
+
+type OrderStatusViewProps = {
+  status: OrderStatus;
+  seatNumber: number;
+};
+
+type ActionButtonProps = {
+  desctructive?: boolean;
+  text: string;
+  onClick: () => void;
+};
+
+export function OrderStatusView({ status, seatNumber }: OrderStatusViewProps) {
+  const clearOrderState = useOrderStore((state) => state.clearState);
+  const router = useRouter();
+
+  const os = OrderStatus;
+
+  let icon,
+    statusHeader,
+    statusDesc,
+    orderSummary,
+    button = null;
+
+  const cancelOrder = () => {};
+
+  const backToMenu = () => {
+    clearOrderState();
+
+    router.push("/menu");
+  };
+
+  const ActionButton = ({
+    desctructive = false,
+    text,
+    onClick,
+  }: ActionButtonProps) => {
+    return (
+      <Button
+        variant={`${desctructive ? "danger" : "primary"}`}
+        className="w-[80%]"
+        onClick={onClick}
+      >
+        {text}
+      </Button>
+    );
+  };
+
+  switch (status) {
+    case os.SUBMITTED:
+      icon = <ClockIcon />;
+      statusHeader = "Order Submitted";
+      statusDesc =
+        "Your order has been submitted and, is waiting to be seen by the kitchen staff";
+      orderSummary = <OrderSummary />;
+      button = (
+        <ActionButton desctructive text="Cancel Order" onClick={cancelOrder} />
+      );
+      break;
+    case os.CONFIRMED:
+      icon = <EyeIcon />;
+      statusHeader = "Order Confirmed";
+      statusDesc =
+        "Your order has been seen by the kitchen staff and will be prepared soon";
+      orderSummary = <OrderSummary />;
+      break;
+    case os.PREPARING:
+      icon = <ChefHatIcon />;
+      statusHeader = "Preparing";
+      statusDesc =
+        "Your order is currently being prepared by our kitchen staff";
+      orderSummary = <OrderSummary />;
+      break;
+    case os.READY:
+      icon = <SoupIcon />;
+      statusHeader = "Order Ready";
+      statusDesc = `Your order is ready and will be served to seat ${seatNumber} shortly`;
+      orderSummary = <OrderSummary />;
+      break;
+    case os.DELIVERED:
+      icon = <CircleCheckIcon />;
+      statusHeader = "Order Complete";
+      statusDesc = `Your order has been delivered to seat ${seatNumber}. Enjoy your meal!`;
+      button = <ActionButton text="Order More" onClick={backToMenu} />;
+      break;
+    case os.REJECTED:
+      icon = <BanIcon />;
+      statusHeader = "Order Rejected";
+      statusDesc =
+        "Your order has been rejected by the staff. Please ask the staff for more information";
+      orderSummary = <OrderSummary idOnly />;
+      button = <ActionButton text="Back to Menu" onClick={backToMenu} />;
+      break;
+    case os.CANCELLED:
+      icon = <XIcon />;
+      statusHeader = "Order Cancelled";
+      statusDesc =
+        "Your order has been cancelled and there for will not be seen by the staff";
+      button = <ActionButton text="Back to Menu" onClick={backToMenu} />;
+      break;
+  }
+
+  return (
+    <div className="flex flex-col flex-1 gap-5 justify-center items-center">
+      {icon}
+      <Typography as="h1" variant="heading-3">
+        {statusHeader}
+      </Typography>
+      <Typography as="p" className="-mt-2 w-[80%] text-center" color="muted">
+        {statusDesc}
+      </Typography>
+      {orderSummary}
+      {button}
+    </div>
+  );
+}
