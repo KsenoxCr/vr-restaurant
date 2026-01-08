694608c42450dc5086e8e9e3c6e4af2865ac41dd
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2026-01-08 20:33:14 +0200

    fix: BackButton navigation (logic inexhaustive edge case wise)

--- src/app/_components/behavior/back-button.tsx
+++ src/app/_components/behavior/back-button.tsx
@@ -2,15 +2,25 @@
 
 import { ArrowLeft } from "lucide-react";
 import { Button } from "~/app/_components/ui/button";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-export function BackButton() {
+export function BackButton({ fallbackHref }: { fallbackHref: string }) {
+  const router = useRouter();
+
   return (
-    <Button variant="ghost" active={false} className="shadow-none">
+    <Button
+      variant="ghost"
+      active={false}
+      className="text-lg shadow-none"
+      onClick={() => {
+        //FIX: Needs conditional logic for handling edge cases where user navigatred from outside the app
+        history.length > 0
+          ? router.back()
+          : router.push(fallbackHref ?? "/menu");
+      }}
+    >
       <ArrowLeft className="w-8 h-8" />
-      <Link className="text-lg" href="/menu">
-        Back
-      </Link>
+      Back
     </Button>
   );
 }
