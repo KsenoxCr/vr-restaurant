b3f9dff983e2478290d39516952262ae7bc0b34f
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2026-01-09 14:37:22 +0200

    feat: kitchen dir structure
    feat: kitchen dir structure

---
+++ src/app/kitchen/login/page.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "~/app/_components/ui/typography";
+
+export function KitchenLogin() {
+  return (
+    <div className="flex flex-col h-screen bg-gray">
+      <div></div>
+      <Typography>Kitchen Terminal</Typography>
+      <Typography>Enter PIN to access</Typography>
+      <div></div>
+    </div>
+  );
+}
