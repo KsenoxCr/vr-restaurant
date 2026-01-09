a21be4bbf58c447cb671dd2894deca79caca7c47
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2026-01-09 16:33:40 +0200

    refactor: validatePin as mutation to remove autofetching, statefulness

--- src/server/api/routers/kitchen.ts
+++ src/server/api/routers/kitchen.ts
@@ -4,7 +4,7 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 export const kitchenRouter = createTRPCRouter({
   validatePin: publicProcedure
     .input(z.number().int().positive().lte(9999))
-    .query(async ({ input }) => {
+    .mutation(async ({ input }) => {
       return input.toString(4) === process.env.KITCHEN_PIN;
     }),
 });
