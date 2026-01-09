4d55e4eac31fb81bdff02a09614a2d3bfcae9b41
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2026-01-09 17:01:28 +0200

    refactor(validatePin): input as string

--- src/server/api/routers/kitchen.ts
+++ src/server/api/routers/kitchen.ts
@@ -1,10 +1,17 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const kitchenRouter = createTRPCRouter({
   validatePin: publicProcedure
-    .input(z.number().int().positive().lte(9999))
+    .input(z.string().length(4))
     .mutation(async ({ input }) => {
-      return input.toString(4) === process.env.KITCHEN_PIN;
+      if (!/^\d+$/.test(input))
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "PIN: digits only",
+        });
+
+      return input === process.env.KITCHEN_PIN;
     }),
 });
