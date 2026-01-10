e7dedab86c35d5ae61f270bb0128d17b4dd5e970
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2026-01-10 15:23:33 +0200

    chore: kitchen.validatePin conditionally creates new session & sets cookie

--- src/server/api/routers/kitchen.ts
+++ src/server/api/routers/kitchen.ts
@@ -1,19 +1,59 @@
+import { SessionRole } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
+import { serialize } from "cookie";
 import { z } from "zod";
+import { newExpiresAt } from "~/lib/utils";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const kitchenRouter = createTRPCRouter({
   validatePin: publicProcedure
     .input(z.string().length(4))
-    .mutation(async ({ input }) => {
+    .mutation(async ({ input, ctx }) => {
       if (!/^\d+$/.test(input))
         throw new TRPCError({
           code: "BAD_REQUEST",
           message: "PIN: digits only",
         });
 
+      const pinCorrect = input === process.env.KITCHEN_PIN;
+
+      if (pinCorrect && (!ctx.session || ctx.session.role !== "KITCHEN")) {
+        const session = await ctx.db.session.create({
+          data: {
+            seatNumber: -1,
+            expiresAt: newExpiresAt(480),
+            role: SessionRole.KITCHEN,
+          },
+        });
+
+        const isProd = process.env.NODE_ENV === "production";
+
+        const cookieName = isProd ? "__Host-session" : "session";
+        const cookieValue = session.id;
+
+        ctx.headers.append(
+          "Set-Cookie",
+          serialize(cookieName, cookieValue, {
+            httpOnly: true,
+            secure: isProd,
+            sameSite: "lax",
+            path: "/",
+            maxAge: 60 * 60 * 24 * 7,
+          }),
+        );
+
+        return {
+          success: pinCorrect,
+          session: {
+            sessionId: session.id,
+            expiresAt: session.expiresAt,
+            role: session.role,
+          },
+        };
+      }
+
       return {
-        success: input === process.env.KITCHEN_PIN,
+        success: pinCorrect,
       };
     }),
 });
