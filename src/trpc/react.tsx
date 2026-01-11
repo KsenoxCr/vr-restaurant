5058f6001bf24f1cb8a9243519d3d7c751715884
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2026-01-11 08:41:31 +0200

    chore: splitLink so cookie setting mutations -> non-streaming, others -> streaming

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { QueryClientProvider, type QueryClient } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
+import {
+  httpBatchLink,
+  loggerLink,
+  splitLink,
+  unstable_httpBatchStreamLink,
+} from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import { useState } from "react";
@@ -36,6 +41,37 @@ export type RouterInputs = inferRouterInputs<AppRouter>;
  */
 export type RouterOutputs = inferRouterOutputs<AppRouter>;
 
+const createHeaders = () => {
+  const h = new Headers();
+  h.set("x-trpc-source", "nextjs-react");
+  return h;
+};
+
+const linkOpts = {
+  transformer: SuperJSON,
+  url: getBaseUrl() + "/api/trpc",
+  headers: createHeaders,
+};
+
+const streaming = unstable_httpBatchStreamLink(linkOpts);
+const nonStreaming = httpBatchLink(linkOpts);
+
+const link = splitLink({
+  condition(op) {
+    const nonStreamingPaths = ["session.create", "kitchen.validatePin"];
+
+    for (const path of nonStreamingPaths) {
+      if (op.path === path) {
+        return true;
+      }
+    }
+
+    return false;
+  },
+  true: nonStreaming,
+  false: streaming,
+});
+
 export function TRPCReactProvider(props: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
 
@@ -47,17 +83,9 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             process.env.NODE_ENV === "development" ||
             (op.direction === "down" && op.result instanceof Error),
         }),
-        unstable_httpBatchStreamLink({
-          transformer: SuperJSON,
-          url: getBaseUrl() + "/api/trpc",
-          headers: () => {
-            const headers = new Headers();
-            headers.set("x-trpc-source", "nextjs-react");
-            return headers;
-          },
-        }),
+        link,
       ],
-    })
+    }),
   );
 
   return (
